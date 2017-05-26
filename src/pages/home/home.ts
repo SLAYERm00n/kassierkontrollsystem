5352f552111260e6a75eb286a357283f811699ba
SLAYERm00n <brotmitkaese@gmail.com>
2017-05-26 18:03:42 +0200

    Initial

---
+++ src/pages/home/home.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { BarcodeScanner, BarcodeScannerOptions } from '@ionic-native/barcode-scanner'
+
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+options: BarcodeScannerOptions;
+results: {};
+
+  constructor(public navCtrl: NavController, private barcode: BarcodeScanner) {
+
+  }
+async scanBarcode() {
+  this.results = await this.barcode.scan();
+  console.log(this.results);
+}
+}

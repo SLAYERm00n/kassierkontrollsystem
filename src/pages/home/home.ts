ebaa022e11d456d370f9571ad3814e900b1dbe3a
SLAYERm00n <brotmitkaese@gmail.com>
2017-05-28 17:03:28 +0200

    Obejkt zu Objekt in home.ts

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -18,8 +18,12 @@ results: {};
 async scanBarcode() {
   this.results = await this.barcode.scan();
   var toJason = this.results["text"];
+  console.log("toJason");
+  console.log(toJason); 
   var jsonData = JSON.parse(toJason);
-  console.log(toJason);  
+  console.log("jsonData");
+  console.log(jsonData);
+   
 }
 /*
 parseToJson(){

eb3fdac693a2505ec49c13db5fe12d9d4cb6984d
Asparo <Robin.angst@hs-furtwangen.de>
2017-06-06 20:27:52 +0200

    SetRoot implemented and some comments for trying to make Guthaben work

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,14 +6,15 @@ import { Goodbyescreen  } from '../goodbyescreen/goodbyescreen';
 import { StaffLogin } from '../staff-login/staff-login';
 import { PurchaseCheck } from '../purchase-check/purchase-check';
 import { Checkscreen } from '../checkscreen/checkscreen';
-
+import { User } from '../../providers/user';
+//import { AuthProviders, AngularFire, FirebaseAuthState, AuthMethods, FirebaseApp } from 'angularfire2';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+useridget : any;
 options: BarcodeScannerOptions;
 result: Object;
 products: any;
@@ -22,7 +23,8 @@ fullName: string;
 sumAktProducts: number = 0;
 scanned: boolean = false;
 guthaben: number = 20;
-
+uid:any;
+userRef:any;
   constructor(public navCtrl: NavController, private barcode: BarcodeScanner) {
 
   }
@@ -40,7 +42,22 @@ async scanBarcode() {
 
   
   })
-if(this.user != null){
+if(this.user != null){  
+
+//this.useridget = new User(this.user.uid);
+
+ /* this.userRef = firebase.database().ref('users/' + this.user.uid);
+    
+    console.debug("Load user using uid " + this.constructor.name);
+    this.userRef.on('value', (snapshot) => {
+      if (snapshot.val() === null) { return; }
+      this.guthaben = snapshot.val().amount});
+*/
+
+
+//this.guthaben = this.useridget.User.amount;
+
+
 
 if(this.guthaben > this.sumAktProducts){
                 this.goToGoodbyescreen();

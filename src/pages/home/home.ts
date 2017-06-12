ef21876a92929306479ec91f7cc6fb054fbcef29
SLAYERm00n <brotmitkaese@gmail.com>
2017-06-12 19:13:16 +0200

    Unuesd imports aus home.ts entfernt

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { BarcodeScanner, BarcodeScannerOptions } from '@ionic-native/barcode-scanner'
+import { BarcodeScanner } from '@ionic-native/barcode-scanner'
 import { Sumscreen  } from '../sumscreen/sumscreen';
 import { Goodbyescreen  } from '../goodbyescreen/goodbyescreen';
-import { StaffLogin } from '../staff-login/staff-login';
-import { PurchaseCheck } from '../purchase-check/purchase-check';
 import { Checkscreen } from '../checkscreen/checkscreen';
 import { User } from '../../providers/user';
 import { DataProvider } from '../../providers/dataprovider';
-//import { AuthProviders, AngularFire, FirebaseAuthState, AuthMethods, FirebaseApp } from 'angularfire2';
 
 @Component({
   selector: 'page-home',
@@ -26,14 +23,11 @@ data: any;
   constructor(public navCtrl: NavController, private barcode: BarcodeScanner, private dataPrv: DataProvider) {
     this.dataPrv.loginWithEmail({email: 'statususer@shoppingapp.de', password: 'status1234'}).subscribe(data =>{
       console.log("Login erfolgreich")
-
     })
   }
 
 
   async scanBarcode() {
-
-
     await this.barcode.scan().then(data =>{
       this.result = JSON.parse(data['text']);
       this.products = this.result['products'];
@@ -45,7 +39,6 @@ data: any;
         products: this.products,
         summe: this.sumAktProducts
       }
-
       this.dataPrv.setData(this.data).then(result =>{
         var user = result;
         if(user != null){  
